55a5dfdf081a90470ee5764828ad2e15ba15b2a6
Celia Garcia <fgarciacelia@gmail.com>
2025-09-20 15:43:29 +0200

    Diseño proyectos actualizado.

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ let menu = document.querySelector('.menu');
 hamburguesa.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+let titulo = document.getElementById("titulo");
+let texto = `Hola, soy Celia, <br> desarrolladora web en formación.`;
+let i = 0;
+
+let cursor = document.createElement("span");
+cursor.classList.add("cursor");
+
+titulo.innerHTML = "";
+titulo.appendChild(cursor);
+
+function typing() {
+  if (i <= texto.length) {
+    titulo.innerHTML = texto.substring(0, i);
+    titulo.appendChild(cursor);
+    i++;
+    setTimeout(typing, 80);
+  } else {
+    titulo.innerHTML = texto; 
+  }
+}
+
+window.onload = typing
