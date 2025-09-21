b94c292f4ba24d926091aef75affdf2ffda0f13b
Celia Garcia <fgarciacelia@gmail.com>
2025-09-21 20:19:39 +0200

    Mensaje de estado de envío de formulario.

--- script.js
+++ script.js
@@ -27,3 +27,27 @@ function typing() {
 }
 
 window.onload = typing
+
+
+let form = document.querySelector(".contact-form");
+let status = document.getElementById("form-reply");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  let data = new FormData(form);
+  try {
+    let response = await fetch(form.action, {
+      method: form.method,
+      body: data,
+      headers: { 'Accept': 'application/json' }
+    });
+    if (response.ok) {
+      status.innerHTML = "Tu mensaje ha sido enviado con éxito. ✅";
+      form.reset();
+    } else {
+      status.innerHTML = "Hubo un error al enviar. Inténtalo de nuevo. ❌ ";
+    }
+  } catch (error) {
+    status.innerHTML = "Error de conexión. ⚠️ ";
+  }
+});
